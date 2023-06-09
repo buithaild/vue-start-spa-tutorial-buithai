91d813e6df3569342dd41a66441dcb0389b9e9d1
buithai <buithaild230899@gmail.com>
2023-06-09 23:19:45 +0700

    Starting project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>Simple text</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activePage: 0,
+
+      pages: [
+        {
+          link: { text: "Home", url: "home.html" },
+          pageTitle: "Home Page",
+          content: "This is the home content",
+        },
+        {
+          link: { text: "About", url: "about.html" },
+          pageTitle: "About",
+          content: "This is the about content",
+        },
+        {
+          link: { text: "Contact", url: "contact.html" },
+          pageTitle: "Contact",
+          content: "This is the contact content",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,3 @@
+import { createApp, VueElement } from "vue";
+import App from "./App";
+createApp(App).mount("#app");
